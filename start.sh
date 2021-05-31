160b3d5d0aa63672b2304b26109f308a52275eca
WarrDoge <74409348+WarrDoge@users.noreply.github.com>
2021-05-31 14:00:50 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ docker build ./ -t chef-server \
 	&& echo "image build!" \
 	&& docker run -d --privileged \
 	--name chef-server  \
-	-p 80:80  \
+	-p 443:443  \
 	chef-server \
 	&& echo "container started!"
 
